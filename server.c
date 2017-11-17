9bf1b48d57752ba5fe6b282abcf06ef482b76721
michaelburke1 <michaeljburke18@gmail.com>
2017-11-17 13:27:25 -0500

    files

--- server.c
+++ server.c
@@ -14,6 +14,7 @@
 #define MAX_THREADS 10
 
 int clients[MAX_THREADS] = {0};
+const char * Usernames[MAX_THREADS];
 int currClient = 0;
 int nUsernames = 0;
 const char * usernames[100];
@@ -23,6 +24,11 @@ const char * passwords[100];
 //the thread function
 void *connection_handler(void *);
 int checkUsername(const char *);
+void private(const char *);
+void broadcast(int);
+const char * getUsers(const char *);
+int writeUP(const char *, const char *);
+const char * getPassword(const char *);
 
 int main(int argc , char *argv[])
 {
@@ -93,17 +99,100 @@ int main(int argc , char *argv[])
      
     return 0;
 }
- 
-int checkUsernames(const char * uname) {
 
+int writeUP(const char * username, const char * password) {
+
+    char fileInput[100];
+
+    FILE *fp;
+    fp=fopen("users.txt", "a");
+    strcpy(fileInput, username);
+    strcat(fileInput, ":");
+    strcat(fileInput, password);
+    strcat(fileInput, "\n");
+    fwrite(fileInput, 1, strlen(fileInput), fp);
+    fclose(fp);
+
+    return 0;
+}
+
+const char * getPassword(const char * username) {
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("users.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        //printf("Retrieved line of length %zu :\n", read);
+        //printf("%s\n", line);
+        char *ch1;
+        ch1 = strtok(line, ":");
+        
+        char name[100];
+        strcpy(name, ch1);
+        ch1 = strtok(NULL, ":");
+        printf("ch1: %s\n", ch1);
+        printf("uname: %s, ch1: %s\n", username, name);
+        if (strcmp(username, name) == 0) {
+            fclose(fp);
+            if (line)
+                free(line);
+            printf("password: %s\n", ch1);
+            return ch1;
+        }
+        //printf("password\n");
+        //printf("%s\n", ch);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    printf("no password found\n");
+    return "what";
+
+}
+
+const char * getUsers(const char * currUser) {
     int i;
 
-    for (i = 0; i < nUsernames; i++) {
-        if (strcmp(uname, usernames[i]) == 0) {
-            return i;
+//    for (i = 0; i < )
+}
+
+int checkUsernames(const char * uname) {
+
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("users.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+//        printf("%s\n", line);
+        char *ch;
+        ch = strtok(line, ":");
+        //printf("%s\n", ch);
+        printf("ch: %s, uname: %s\n", ch, uname);
+        if (strcmp(ch, uname) == 0) {
+            fclose(fp);
+            if (line)
+                free(line);
+            printf("user exists\n");
+            return 1;
         }
     }
-    
+
+    fclose(fp);
+    if (line)
+        free(line);
+   
+    printf("user does not exist\n");
     return -1;
 }
 
@@ -113,9 +202,6 @@ int checkUsernames(const char * uname) {
 void *connection_handler(void *socket_desc)
 {
 
-    usernames[nUsernames] = "marv";
-    passwords[nUsernames] = "smarv";
-    nUsernames++;
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     int read_size;
@@ -134,7 +220,10 @@ void *connection_handler(void *socket_desc)
     printf("message: %s\n", client_message);
     
     int check = checkUsernames(client_message);
-    const char * thisUser = client_message;
+    char thisUser[100];
+    strcpy(thisUser, client_message);
+    printf("this user is %s\n", thisUser);
+    
     if (check == -1) {
         printf("Welcome new user!\n");     
         printf("Enter a password for us to save!\n");
@@ -153,7 +242,7 @@ void *connection_handler(void *socket_desc)
         printf("Check: %d\n", check);
         printf("returning user\n");
         while(1) {
-            if (strcmp(client_message, passwords[check]) == 0) {
+            if (strcmp(client_message, getPassword(thisUser)) == 0) {
                 printf("password matched\n");
                 break;
             } else {
@@ -165,14 +254,13 @@ void *connection_handler(void *socket_desc)
             }
         }
     } else {
-        usernames[nUsernames] = thisUser;
         printf("new user, creating password\n");
-        passwords[nUsernames] = client_message;
-        printf("password created for user: %s\npassword: %s\n", usernames[nUsernames], passwords[nUsernames]);
-        nUsernames++;
+        printf("password created for user: %s\npassword: %s\n", thisUser, client_message);
+        writeUP(thisUser, client_message);
     }
-
+    
     message = "SUCCESS";
+    Usernames[currClient - 1] = thisUser;
     write(sock, message, strlen(message));
     memset(client_message, 0, 2000);
 
@@ -185,20 +273,36 @@ void *connection_handler(void *socket_desc)
     memset(client_message, 0, 2000);
 
 
-    message = "Now type something and i shall repeat what you type \n";
-    write(sock , message , strlen(message));
+    const char * prompt = "Let's chat!\n\n\n";
+
+    write(sock , prompt, strlen(prompt));
      
     //Receive a message from client
     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
     {
         //end of string marker
 		client_message[read_size] = '\0';
-		
+	
+        if (strcmp(client_message, "B") == 0) {
+            printf("broadcast\n");
+            message = "broad\n";
+        } else if (strcmp(client_message, "P") == 0) {
+            printf("private message\n");
+            message = "private message, input user to chat\n";
+        } else if (strcmp(client_message, "E") == 0) {
+            printf("exit\n");
+            message = "exit\n";
+        } else {
+            printf("incorrect input\n");
+            message = "incorrect input\n";
+        }
+
 		//Send the message back to client
-        write(sock , client_message , strlen(client_message));
-		
-		//clear the message buffer
+        write(sock , message, strlen(message));
 		memset(client_message, 0, 2000);
+
+        write(sock , prompt, strlen(prompt));
+        memset(client_message, 0, 2000);
     }
      
     if(read_size == 0)
